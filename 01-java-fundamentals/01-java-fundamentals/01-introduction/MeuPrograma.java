f2c4397cfd08769c449cedd296cad91a0e8e47be
alexbleggijrr <alexbleggijrr@gmail.com>
2026-06-03 15:28:31 -0300

    installing the jdk

---
+++ 01-java-fundamentals/01-java-fundamentals/01-introduction/MeuPrograma.java
@@ -0,0 +1,7 @@
+// javac MeuPrograma.java => 'MeuPrograma.class' => java MeuPrograma
+
+public class MeuPrograma {
+  public static void main (String[] args) {
+    System.out.print("Codigo Java");
+  }
+}
