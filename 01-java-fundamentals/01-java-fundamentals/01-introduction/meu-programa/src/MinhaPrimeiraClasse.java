c87ac0f319e576e98c63a98f9eb400722d156e09
alexbleggijrr <alexbleggijrr@gmail.com>
2026-06-04 11:13:16 -0300

    variables and methods concepts

--- 01-java-fundamentals/01-java-fundamentals/01-introduction/meu-programa/src/MinhaPrimeiraClasse.java
+++ 01-java-fundamentals/01-java-fundamentals/01-introduction/meu-programa/src/MinhaPrimeiraClasse.java
@@ -1,5 +1,10 @@
 public class MinhaPrimeiraClasse {
   public static void main(String[] args) {
-    System.out.println("Meu codigo escrito com uma IDE");
+    // constante
+    final String BR_PAIS = "Brasil";
+
+    // variável
+    String brPais = "Brasil";
+
   }
 }
